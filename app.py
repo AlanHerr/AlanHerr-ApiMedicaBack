60f04a2b44876a64e5fdcacf748c622663e8c1f3
Alan Herrera <102233902+AlanHerr@users.noreply.github.com>
2025-11-05 02:55:45 +0000

    Elimina el registro del blueprint de productos y añade el de diabetes en la aplicación Flask.

--- app.py
+++ app.py
@@ -3,13 +3,13 @@
 
 import os
 from flask import Flask
-from controller.products_controller import products_bp
 from controller.user_controller import users_bp
+from controller.diabetes_controller import diabetes_bp
 from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
-app.register_blueprint(products_bp)
 app.register_blueprint(users_bp)
+app.register_blueprint(diabetes_bp)
 
 # Configuración de la clave secreta para JWT
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "tu_clave_secreta_jwt")
