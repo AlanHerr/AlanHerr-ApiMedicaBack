e9e08b9d6b434acda341c5ed4540643e319834db
Alan Herrera <102233902+AlanHerr@users.noreply.github.com>
2026-03-17 14:03:16 +0000

    Añade el campo is_admin al modelo User para gestionar permisos de administrador

--- model/user.py
+++ model/user.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from model.base import Base
 import logging
 
@@ -10,6 +10,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String(80), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
+    is_admin = Column(Boolean, default=False, nullable=False)
 
     def __repr__(self):
         logger.info(f'Representación de usuario solicitada: {self.username}')
