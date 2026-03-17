4c2b93a04ffcf9d72a0ddf7194120546c3ec5704
Alan Herrera <102233902+AlanHerr@users.noreply.github.com>
2026-03-17 14:03:52 +0000

    Añade el método get_by_id al repositorio UserRepository para obtener usuarios por ID

--- repository/user_repository.py
+++ repository/user_repository.py
@@ -3,6 +3,13 @@ from config.database import get_db_session
 
 class UserRepository:
     @staticmethod
+    def get_by_id(user_id):
+        session = get_db_session()
+        user = session.query(User).filter_by(id=user_id).first()
+        session.close()
+        return user
+
+    @staticmethod
     def get_by_username(username):
         session = get_db_session()
         user = session.query(User).filter_by(username=username).first()
